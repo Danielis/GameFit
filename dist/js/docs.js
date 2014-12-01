2fc3a7e29a38e753edff69fdb0cf77bf57b5be9c
danielCantwell <dcantwel@usc.edu>
2014-11-30 19:34:01 -0800

    filled in documentation page

---
+++ dist/js/docs.js
@@ -0,0 +1,19 @@
+
+function updateContent(e) {
+    var pageContent = document.getElementById("page-content-wrapper");
+
+    switch (e) {
+        case "dashboard":
+            pageContent.innerHTML = "DASHBOARD";
+            break;
+        case "getting-started":
+            pageContent.innerHTML = "<iframe src='docs_getting_started.html' frameborder=\"no\" width=\"100%\" height=\"100%\" scrolling=\"no\"></iframe>";
+            break;
+        case "pc-games":
+            pageContent.innerHTML = "<iframe src='docs_pc_games.html' frameborder=\"no\" width=\"100%\" height=\"100%\" scrolling=\"no\"></iframe>";
+            break;
+        case "mobile-games":
+            pageContent.innerHTML = "<iframe src='docs_mobile_games.html' frameborder=\"no\" width=\"100%\" height=\"100%\" scrolling=\"no\"></iframe>";
+            break;
+    }
+}
