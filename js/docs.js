6356b6a650b4ae8f484fdbd411c9318bfe63e23a
Daniel Silva <Danielis@usc.edu>
2014-12-15 10:14:37 -0800

    Changed filler text and added new images, modified demo

--- js/docs.js
+++ js/docs.js
@@ -3,16 +3,16 @@ function updateContent(e) {
 
     switch (e) {
         case "overview":
-            pageContent.innerHTML = "THIS IS AN OVERVIEW";
+            pageContent.innerHTML = "<iframe src='docs_overview.html' frameborder=\"no\" width=\"100%\" height=\"100%\" scrolling=\"no\"></iframe>";
             break;
         case "getting-started":
             pageContent.innerHTML = "<iframe src='docs_getting_started.html' frameborder=\"no\" width=\"100%\" height=\"100%\" scrolling=\"no\"></iframe>";
             break;
-        case "pc-games":
-            pageContent.innerHTML = "<iframe src='docs_pc_games.html' frameborder=\"no\" width=\"100%\" height=\"100%\" scrolling=\"no\"></iframe>";
+        case "reference":
+            pageContent.innerHTML = "<iframe src='docs_reference.html' frameborder=\"no\" width=\"100%\" height=\"100%\" scrolling=\"no\"></iframe>";
             break;
-        case "mobile-games":
-            pageContent.innerHTML = "<iframe src='docs_mobile_games.html' frameborder=\"no\" width=\"100%\" height=\"100%\" scrolling=\"no\"></iframe>";
-            break;
+        case "demo":
+        pageContent.innerHTML = "<iframe src='demo.html' frameborder=\"no\" width=\"100%\" height=\"100%\" scrolling=\"no\"></iframe>";
+        break;
     }
 }
